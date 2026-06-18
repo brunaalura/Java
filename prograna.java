bdcf2aeb20c9d2efd3cc2fdd7d65a424994417f3
brunaalura <134618759+brunaalura@users.noreply.github.com>
2026-06-18 08:44:41 -0300

    programa

---
+++ prograna.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Ana";
+        int idade = 16;
+
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
